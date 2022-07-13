70e0a4dd1db080959981af60963c63746ff4ffe6
LeVanHang <vanhang1802@gmail.com>
2022-07-13 19:42:44 +0700

    ifelse

---
+++ ifelse11/b1.js
@@ -0,0 +1,8 @@
+//Bài 1: Nhập vào hai số a và b, và kiểm tra xem a có chia hết cho b hay không
+let a = prompt(parseInt('input'));
+let b =prompt(parseInt('input'));
+if(a%b==0){
+    alert('true')
+}else {
+    alert("no no")
+}
---
+++ ifelse11/b2.js
@@ -0,0 +1,8 @@
+//Bài 2: Nhập tuổi và in ra kết quả
+// nếu tuổi học sinh đó không đủ điều kiện vào học lớp 10.
+let year = +prompt('input year now');
+if(year<=15){
+    document.write('không đủ điều kiện vào lớp 10')
+} else {
+    document.write('chào mừng đến với lớp 10')
+}
